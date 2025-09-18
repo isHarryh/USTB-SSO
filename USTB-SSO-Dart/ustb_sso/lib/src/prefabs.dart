3d85b5abf825851a990ff702a9f87312c95daaf2
Harry Huang <harryhuang2652@qq.com>
2025-09-18 22:11:38 +0800

    feat(dart): add byyt platform

--- USTB-SSO-Dart/ustb_sso/lib/src/prefabs.dart
+++ USTB-SSO-Dart/ustb_sso/lib/src/prefabs.dart
@@ -22,18 +22,25 @@ class ApplicationParam {
 
 /// Pre-configured application parameters for common USTB services.
 class Prefabs {
-  /// 北科大教务管理系统
+  /// 北京科技大学教务管理系统 2022 年版（已于 2025 年弃用）
   static const ApplicationParam jwglUstbEduCn = ApplicationParam(
     entityId: 'NS2022062',
     redirectUri: 'https://jwgl.ustb.edu.cn/glht/Logon.do?method=weCharLogin',
     state: 'test',
   );
 
-  /// 北科大AI助手 (2025年版)
+  /// 北京科技大学AI助手聊天系统 2025 年版
   static const ApplicationParam chatUstbEduCn = ApplicationParam(
     entityId: 'YW2025007',
     redirectUri:
         'http://chat.ustb.edu.cn/common/actionCasLogin?redirect_url=http%3A%2F%2Fchat.ustb.edu.cn%2Fpage%2Fsite%2FnewPc%3Flogin_return%3Dtrue',
     state: 'ustb',
+  );
+
+  /// 北京科技大学本研一体教务管理系统 2025 年版
+  static const ApplicationParam byytUstbEduCn = ApplicationParam(
+    entityId: 'YW2025006',
+    redirectUri: 'https://byyt.ustb.edu.cn/oauth/login/code',
+    state: 'null',
   );
 }
